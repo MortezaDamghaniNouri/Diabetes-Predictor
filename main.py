7df43c28abd07b4e3c6d9f0438e957ea74a56659
Morteza Damghani <mortezadamghani77@gmail.com>
2022-09-04 10:11:45 -0700

    Cleaned dataset list generated.

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ for name in column_names:
     temp = dataset[dataset[name].isnull()].index.tolist()
     for i in temp:
         null_indexes.append(i)
-
-
-
-
-
-
+i = 0
+cleaned_dataset_list = []
+while i < len(dataset_list):
+    if i not in null_indexes:
+        cleaned_dataset_list.append(dataset_list[i])
+    i += 1
 
 
 
