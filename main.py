befbf45a64df17dba8643a9fea596dbe7f3a4825
Morteza Damghani <mortezadamghani77@gmail.com>
2022-09-04 11:43:55 -0700

    The periods of different variables found

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import pandas as pd
-import numpy as np
+
+# This function sorts the input numerical list by bubble sort
+def sorter(input_list):
+    k = len(input_list)
+    while k != 1:
+        m = 0
+        while m < (k-1):
+            if input_list[m] > input_list[m + 1]:
+                template = input_list[m + 1]
+                input_list[m + 1] = input_list[m]
+                input_list[m] = template
+            m += 1
+        k = k - 1
+
+
+# This function removes repetitious values in the input list
+def repetition_remover(input_list):
+    template = []
+    for k in input_list:
+        if k not in template:
+            template.append(k)
+    return template
+
+
 dataset = pd.read_csv("Dataset/diabetes.csv")
 dataset_list = dataset.values.tolist()
 # Finding the indexes of Nan values
@@ -18,6 +41,20 @@ while i < len(dataset_list):
     if i not in null_indexes:
         cleaned_dataset_list.append(dataset_list[i])
     i += 1
+null_indexes = []
+i = 0
+while i < len(cleaned_dataset_list):
+    if cleaned_dataset_list[i][22] == "Unknown":
+        null_indexes.append(i)
+    i += 1
+new_cleaned_dataset = []
+i = 0
+while i < len(cleaned_dataset_list):
+    if i not in null_indexes:
+        new_cleaned_dataset.append(cleaned_dataset_list[i])
+    i += 1
+cleaned_dataset_list = new_cleaned_dataset
+
 
 # Removing white spaces in different tuples
 i = 0
@@ -29,9 +66,14 @@ while i < len(cleaned_dataset_list):
         j += 1
     i += 1
 
-
-
-
+BMIs = []
+i = 0
+while i < len(cleaned_dataset_list):
+    BMIs.append(cleaned_dataset_list[i][20])
+    i += 1
+BMIs = repetition_remover(BMIs)
+sorter(BMIs)
+print(BMIs)
 
 
 
