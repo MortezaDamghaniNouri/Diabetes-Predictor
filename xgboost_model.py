a77892d24289b5e20618d99c9cad2781c2e8d126
Morteza Damghani <mortezadamghani77@gmail.com>
2022-09-05 03:38:01 -0700

    Basic form of the model implemented.

--- xgboost_model.py
+++ xgboost_model.py
@@ -3,10 +3,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 dataset = pd.read_csv("Dataset/cleaned_dataset.csv")
 labels = pd.read_csv("Dataset/cleaned_dataset_labels.csv")
-X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.2, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(dataset, labels, test_size=0.2, random_state=0)
+classifier = xgb.XGBClassifier()
+classifier.fit(X_train, Y_train)
+Y_pred = classifier.predict(X_test)
+Y_test = Y_test.values.tolist()
 
-
-
+# Calculating the precision of the model
+i = 0
+correct_counter = 0
+while i < len(Y_pred):
+    if Y_pred[i] == Y_test[i][0]:
+        correct_counter += 1
+    i += 1
+print("Precision: " + str((correct_counter / len(Y_pred)) * 100))
 
 
 
